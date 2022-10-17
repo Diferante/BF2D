5af84ed1e6a6e2f6c1b6686e264fb47bf69b3d53
Diferante <diogoantunes.gm@gmail.com>
2022-10-18 01:58:04 +0200

    Comments and update direction parameter

--- FindBoundary.py
+++ FindBoundary.py
@@ -8,6 +8,21 @@ norm = np.linalg.norm
 
 
 class BoundaryTracer:
+    # Class to trace edges of a binary image.
+    #
+    # The class creates a squence of pairs of points. Each pair includes a point inside and a point outside the set.
+    # PairTol determines the maximum distance points in a pair.
+    # Each step creates a new pair. To create a new pair, a step is taken in a direction.
+    # Taking a step consists of moving the distance StepLength from the middle of the previous pair in a direction, and then expanding in the
+    # perpendicular direction to create a two points seperated by StepWidth.
+    # If these points are inside and outside as expected, this forms a new pair.
+    # If one of the points is unexpectadly inside/outside the set, then a pair is formed with this new point and a point
+    # from the previous pair, in order to form a pair (which needs an inside and an outside point).
+    # When a new pair is formed it is tightned.
+    # The direction is updated with the actual direction that the paris are moving in (the direction between pair centers),
+    # but keeping a memory of the previous directions. How strong the corrections are is determined by DirectionUpdateFactor.
+    # A value of 0 means no update at all (and the algorithm will never turn) and 1 means no memory from the previous steps.
+
     def __init__(
         self,
         In0: Point,
@@ -16,10 +31,12 @@ class BoundaryTracer:
         StepWidth: float,
         StepLength: float,
         PairTol: float,
+        DirectionUpdateFactor: float,
     ) -> None:
         self.StepWidth = StepWidth
         self.StepLength = StepLength
         self.PairTol = PairTol
+        self.DirectionUpdateFactor = DirectionUpdateFactor
         self.isInside = isInside  # Checks if a point is inside the set
 
         In0, Out0 = self.TightenPair(In0, Out0)
@@ -32,6 +49,7 @@ class BoundaryTracer:
         ) / np.linalg.norm(self.Direction)
 
     def TakeStep(self, Tighten=True) -> None:
+        # Takes a step. If Tighten is False, skips the tightening of the last pair and the updating of Direction.
         StepAlongLine = self.StepLength * self.Direction
         StepOutside = (
             self.StepWidth * np.array([-self.Direction[1], self.Direction[0]]) / 2
@@ -66,7 +84,7 @@ class BoundaryTracer:
         newM = (self.In[-1] + self.Out[-1]) / 2
         Diff = newM - oldM
         n = norm(Diff)
-        k = 0.5
+        k = self.DirectionUpdateFactor
         if n != 0:
             self.Direction = self.Direction * k + (1 - k) * Diff / norm(Diff)
 
