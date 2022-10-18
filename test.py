21da6fcab99d4e01904390e4b63609647afb1429
Diferante <diogoantunes.gm@gmail.com>
2022-10-18 02:16:00 +0200

    updatediection and video name

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import FindBoundary as fb
 
 
 def f(x):
-    return -3 * x**3 + 4 * x**2 + 5 * x + 2 * np.exp(x)
+    return -3 * x**3 + 4 * x**2 + 5 * x + 2 * np.exp(x) + 0.3 * np.sin(30 * x)
 
 
 def isInside(Point):
@@ -21,17 +21,29 @@ def isInside(Point):
 
 In = np.array([0, 15])
 Out = np.array([0, 0])
-StepWidth = 0.1
-StepLength = 0.5
-PairTol = 0.1
-tracer = fb.BoundaryTracer(In, Out, isInside, StepWidth, StepLength, PairTol)
+tracer = fb.BoundaryTracer(
+    In,
+    Out,
+    isInside=isInside,
+    StepWidth=0.1,
+    StepLength=0.5,
+    PairTol=0.1,
+    DirectionUpdateFactor=0.5,
+)
 Start = tracer.GetNewestMiddle()
-fig = plt.figure()
+fig = plt.figure(figsize=(5.12, 10.08), tight_layout=True)
 ax = fig.gca()
-xx = np.linspace(-20, 20, 10001)
-# ax.plot(xx, f(xx), linestyle=":")
-(InPlot,) = ax.plot([0, 0], [0, 1], linewidth=1, linestyle="--")
-(OutPlot,) = ax.plot([0, 0], [0, 1], linewidth=1, linestyle="--")
+# fig2 = plt.figure(figsize=(5, 10))
+# ax2 = fig2.gca()
+# xx = np.linspace(-20, 20, 10001)
+# ax2.fill_between(xx, f(xx), 30)
+# ax2.set_xlim([-10, 10])
+# ax2.set_ylim([0, 20])
+# plt.pause(0.1)
+(InPlot,) = ax.plot([0, 0], [0, 1], linewidth=1, linestyle="", marker=".", markersize=1)
+(OutPlot,) = ax.plot(
+    [0, 0], [0, 1], linewidth=1, linestyle="", marker=".", markersize=1
+)
 InPlot.set_data(np.array(tracer.In).T)
 OutPlot.set_data(np.array(tracer.Out).T)
 ax.set_xlim([-10, 10])
@@ -68,7 +80,7 @@ filenames = []
 
 def save_frame():
     global i
-    plt.pause(1e-9)
+    # plt.pause(1e-9)
     # create file name and append it to a list
     filename = f"{i}.png"
     filenames.append(filename)
@@ -94,7 +106,7 @@ while True:
     autoscale()
 
     M = tracer.GetNewestMiddle()
-    if LeftStart and np.linalg.norm(Start - M) < 0.6 * StepLength:
+    if LeftStart and np.linalg.norm(Start - M) < 0.6 * tracer.StepLength:
         print("Tracing done, back to the start")
         tracer.In.append(tracer.In[0])
         tracer.Out.append(tracer.Out[0])
@@ -106,9 +118,11 @@ while True:
     elif not LeftStart and np.linalg.norm(Start - M) > 5:
         LeftStart = True
 
-
+end_name = "compl6"
+# save final frame
+plt.savefig(end_name + ".png")
 # build gif
-with imageio.get_writer("compl.gif", mode="I") as writer:
+with imageio.get_writer(end_name + ".mp4", mode="I") as writer:
     for filename in filenames:
         image = imageio.v2.imread(filename)
         writer.append_data(image)
